9fed6b0fb6066f49dfa61b5e3b4b532301017606
ma$ha <113903783+Maashaaa@users.noreply.github.com>
2026-02-02 17:04:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,6 +5,10 @@ const FILES_TO_CACHE = [
   './manifest.json'
 ];
 
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
